3945dd3096814c72791bb91ec2c3255d92f613f0
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-01-19 06:10:44 +0100

    done

--- script.js
+++ script.js
@@ -160,14 +160,13 @@
 
 // CODING CHALLENGE 2
 
-function calcTip(billValue) {
-  const tip = billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
-  console.log(tip);
-  console.log(`the bill was ${billValue}, the tip was ${tip}, and the total value ${billValue + tip}`);
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
-calcTip(100);
 
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
 
-console.log(tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
