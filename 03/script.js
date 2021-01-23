be8ec263c3dbfdd87a69a88bb669cc154f0033b7
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-01-23 07:35:59 +0100

    end of chapter

--- 03/script.js
+++ 03/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+let data1 = [17, 21, 23];
+let data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let string = "";
+  for (let i = 0; i < arr.length; i++) {
+    string += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+  console.log(string);
+};
+
+printForecast(data1);
