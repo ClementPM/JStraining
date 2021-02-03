36a02a1d4ecf104ed696ed4ff7a9ac350ccd9e2a
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-02-03 07:07:06 +0100

    condition logic

--- 05/script.js
+++ 05/script.js
@@ -1,12 +1,45 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener(
   'click', function() {
     const guess = Number (document.querySelector('.guess').value);
     console.log(guess);
   
+    // When no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!'
+
+    // When the number is correct
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    
+    // When input is too high
+  } else if (guess > secretNumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'Too high!'
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!'
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // when input is too low
+  } else if (guess < secretNumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = 'Too low!'
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!'
+      document.querySelector('.score').textContent = 0;
+    }
   }
 } );
 
