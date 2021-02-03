adbec0d53c4fd803f50197b33758150080b032da
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-02-03 06:35:21 +0100

    click event

--- 05/script.js
+++ 05/script.js
@@ -1,8 +1,20 @@
 'use strict';
 
+document.querySelector('.check').addEventListener(
+  'click', function() {
+    const guess = Number (document.querySelector('.guess').value);
+    console.log(guess);
+  
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!'
+  }
+} );
+
+/*
 document.querySelector('.message').textContent = 'try me';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 22;
+document.querySelector('.guess').value = 22
+*/
