491340e011eab774673a1a935767eed836241133
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-02-04 06:04:00 +0100

    implemented reset

--- 05/script.js
+++ 05/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.check').addEventListener(
@@ -19,6 +17,7 @@ document.querySelector('.check').addEventListener(
     document.querySelector('.message').textContent = 'Correct Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
     // When input is too high
   } else if (guess > secretNumber) {
@@ -30,7 +29,7 @@ document.querySelector('.check').addEventListener(
       document.querySelector('.message').textContent = 'Game over!'
       document.querySelector('.score').textContent = 0;
     }
-
+    
     // when input is too low
   } else if (guess < secretNumber) {
     if(score > 1){
@@ -43,3 +42,15 @@ document.querySelector('.check').addEventListener(
     }
   }
 } );
+
+// To reset
+
+document.querySelector('.again').addEventListener('click', function() {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#000000';
+});
