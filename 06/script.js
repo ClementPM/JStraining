fd937842c45047914355611e06aae730bc51956b
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-02-05 07:13:28 +0100

    keypress

--- 06/script.js
+++ 06/script.js
@@ -20,3 +20,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// Keypress
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+  }
+});
