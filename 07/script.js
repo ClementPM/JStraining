4cc0e39346907e08e6cf85cfdc4ddfbd7280e4dd
Clement Perier Muzet <barceloneta08003@gmail.com>
2021-02-11 05:36:47 +0100

    game completed

--- 07/script.js
+++ 07/script.js
@@ -11,17 +11,29 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// initializing function
+const init = function() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-  //switch to other player
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player1El.classList.add('player--active');
+  player1El.classList.remove('player--activer');
+};
+init();
+
+//switch to other player
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -31,7 +43,7 @@ const switchPlayer = function () {
   }
 
 //Rolling dice
-btnRoll.addEventListener('click', function() {
+  btnRoll.addEventListener('click', function() {
   if (playing) {
     const dice = Math.trunc(Math.random() * 6) + 1;
     diceEl.classList.remove('hidden');
@@ -53,7 +65,7 @@ btnHold.addEventListener('click', function() {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
     // check if player score >100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -63,3 +75,6 @@ btnHold.addEventListener('click', function() {
     }
   }
 });
+
+// reset the game
+btnNew.addEventListener('click', init);  
